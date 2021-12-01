b532f3ac2e2a1c518ccbd38b1eede964a63099c6
姚非凡 <7370372+yao_extraordinary@user.noreply.gitee.com>
2021-12-01 11:35:16 +0800

    配置组件库

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import naive from './plugins/naive';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+createApp(App)
+.use(naive)
+.mount('#app');
